7cbe7f8c568eab4f0ad72cfa9a082c3fcdf82f4c
bioroid17 <trace1997@ajou.ac.kr>
2024-09-07 17:32:04 +0900

    DOCUMENTGPT - Recap
    Code Challenge: Add Memory

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -1,9 +1,5 @@
-from typing import Dict, List
-from uuid import UUID
-from langchain.schema.output import ChatGenerationChunk, GenerationChunk
-import streamlit as st
 from langchain.chat_models import ChatOpenAI
-from langchain.prompts import ChatPromptTemplate
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.document_loaders import UnstructuredFileLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings, CacheBackedEmbeddings
@@ -11,6 +7,11 @@ from langchain.vectorstores import FAISS
 from langchain.storage import LocalFileStore
 from langchain.schema.runnable import RunnablePassthrough, RunnableLambda
 from langchain.callbacks.base import BaseCallbackHandler
+from langchain.memory import ConversationBufferMemory
+from langchain.schema.messages import AIMessage, HumanMessage
+from langchain.schema import messages_from_dict, messages_to_dict
+import streamlit as st
+import json, os
 
 st.set_page_config(
     page_title="DocumentGPT",
@@ -31,13 +32,6 @@ class ChatCallbackHandler(BaseCallbackHandler):
     def on_llm_new_token(self, token, *args, **kwargs):
         self.message += token
         self.message_box.markdown(self.message)
-
-
-llm = ChatOpenAI(
-    temperature=0.1,
-    streaming=True,
-    callbacks=[ChatCallbackHandler()],
-)
 
 
 @st.cache_data(show_spinner="Embedding file...")
@@ -86,6 +80,38 @@ def format_docs(docs):
     return "\n\n".join(document.page_content for document in docs)
 
 
+def get_history(input):
+    return memory.load_memory_variables({})["history"]
+
+
+def restore_memory(pathname):
+    global memory
+    with open(pathname, "r") as f:
+        memory_json = json.load(f)
+        messages = messages_from_dict(memory_json)
+        for i in range(0, len(messages), 2):
+            if type(messages[i]) == HumanMessage:
+                save_message(messages[i].content, "human")
+            if type(messages[i + 1]) == AIMessage:
+                save_message(messages[i + 1].content, "ai")
+            memory.save_context(
+                {"input": messages[i].content},
+                {"output": messages[i + 1].content},
+            )
+
+
+llm = ChatOpenAI(
+    temperature=0.1,
+    streaming=True,
+    callbacks=[ChatCallbackHandler()],
+)
+
+if "memory" not in st.session_state:
+    memory = ConversationBufferMemory(return_messages=True)
+    st.session_state["memory"] = memory
+else:
+    memory = st.session_state["memory"]
+
 prompt = ChatPromptTemplate.from_messages(
     [
         (
@@ -96,6 +122,7 @@ prompt = ChatPromptTemplate.from_messages(
             Context: {context}
             """,
         ),
+        MessagesPlaceholder(variable_name="history"),
         ("human", "{question}"),
     ]
 )
@@ -120,12 +147,18 @@ with st.sidebar:
 if file:
     retriver = embed_file(file)
     send_message("I'm ready! Ask away!", "ai", save=False)
+
+    pathname = f"./.cache/memories/{file.name}/memory.json"
+    if os.path.exists(pathname) and not st.session_state["messages"]:
+        restore_memory(pathname)
+
     paint_history()
     message = st.chat_input("Ask anything about your file...")
     if message:
         send_message(message, "human")
         chain = (
             {
+                "history": RunnableLambda(get_history),
                 "context": retriver | RunnableLambda(format_docs),
                 "question": RunnablePassthrough(),
             }
@@ -133,6 +166,13 @@ if file:
             | llm
         )
         with st.chat_message("ai"):
-            response = chain.invoke(message)
+            result = chain.invoke(message)
+        memory.save_context({"input": message}, {"output": result.content})
+        dirname = f"./.cache/memories/{file.name}/"
+        if not os.path.exists(dirname):
+            os.mkdir(dirname)
+        with open(pathname, "w") as f:
+            messages = messages_to_dict(get_history({}))
+            json.dump(messages, f)
 else:
     st.session_state["messages"] = []
