54c58266bb9d7ac2fd0d82e7eddf1d18e1de1ad4
bioroid17 <trace1997@ajou.ac.kr>
2024-09-10 20:50:53 +0900

    SITEGPT - Parsing Function

--- pages/04_SiteGPT.py
+++ pages/04_SiteGPT.py
@@ -1,4 +1,5 @@
 from langchain.document_loaders import SitemapLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from fake_useragent import UserAgent
 import streamlit as st
 import asyncio
@@ -24,14 +25,50 @@ st.markdown(
 ua = UserAgent()
 
 
+def parse_page(soup):
+    header = soup.find("header")
+    footer = soup.find("footer")
+    nav = soup.find("nav")
+    aside = soup.find("aside")
+    astro_breadcrumbs = soup.find("astro-breadcrumbs")
+    if header:
+        header.decompose()
+    if footer:
+        footer.decompose()
+    if nav:
+        nav.decompose()
+    if aside:
+        aside.decompose()
+    if astro_breadcrumbs:
+        astro_breadcrumbs.decompose()
+    return (
+        str(soup.get_text())
+        .replace("\n", " ")
+        .replace("\xa0", " ")
+        .replace(
+            "Cloudflare DashboardDiscordCommunityLearning CenterSupport Portal  Cookie Settings",
+            "",
+        )
+    )
+
+
 @st.cache_data(show_spinner="Loading website...")
 def load_website(url):
+    splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+        chunk_size=1000, chunk_overlap=200
+    )
     try:
-        loader = SitemapLoader(url)
-        loader.requests_per_second = 1
+        loader = SitemapLoader(
+            url,
+            filter_urls=[
+                r"^(.*\/encryption\/).*",
+            ],
+            parsing_function=parse_page,
+        )
+        loader.requests_per_second = 5
         # Set a realistic user agent
         loader.headers = {"User-Agent": ua.random}
-        docs = loader.load()
+        docs = loader.load_and_split(text_splitter=splitter)
         return docs
     except Exception as e:
         return []
