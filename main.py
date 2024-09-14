73df1bbc9d1302feb423fd0ceaddd505fb153f91
bioroid17 <trace1997@ajou.ac.kr>
2024-09-14 21:06:02 +0900

    CHEFGPT - API Key Auth

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from pydantic import BaseModel, Field
 
 app = FastAPI(
@@ -29,7 +29,8 @@ class Quote(BaseModel):
     response_model=Quote,
     openapi_extra={"x-openai-isConsequential": False},
 )
-def get_quote():
+def get_quote(request: Request):
+    print(request.headers)
     return {
         "quote": "Life is short so eat it all.",
         "year": 1950,
