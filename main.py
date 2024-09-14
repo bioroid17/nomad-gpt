240b33046b7d590e582fd16464ed4913c7ac2c22
bioroid17 <trace1997@ajou.ac.kr>
2024-09-15 06:25:09 +0900

    CHEFGPT - Chef API

--- main.py
+++ main.py
@@ -1,41 +1,52 @@
-from fastapi import FastAPI, Form, Request
+from fastapi import FastAPI, Form
 from fastapi.responses import HTMLResponse
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
+from dotenv import load_dotenv
+from pinecone import Pinecone
+import os
+from langchain.embeddings import OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
+
+load_dotenv()
+
+pc = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
+
+
+embeddings = OpenAIEmbeddings()
+
+vector_store = PineconeVectorStore.from_existing_index(
+    "recipes",
+    embeddings,
+)
 
 app = FastAPI(
-    title="Nicolacus Maximus Quote Giver",
-    description="Get a real quote said by Nicolacus Maximus himself.",
+    title="ChefGPT. The best provider of Indian Recipes in the world.",
+    description="Give ChefGPT the name of an ingredient and it will give you multiple recipes to use that ingredient on in return.",
     servers=[
         {
-            "url": "https://finance-becomes-carbon-record.trycloudflare.com",
+            "url": "https://instance-polyphonic-drilling-qualification.trycloudflare.com",
         },
     ],
 )
 
 
-class Quote(BaseModel):
-    quote: str = Field(
-        description="The quote that Nicolacus Maximus said.",
-    )
-    year: int = Field(
-        description="The year when Nicolacus Maximus said the quote.",
-    )
+class Document(BaseModel):
+    page_content: str
 
 
 @app.get(
-    "/quote",
-    summary="Returns a random quote by Nicolacus Maximus",
-    description="Upon receiving a GET request this endpoint will return a real quiote said by Nicolacus Maximus himself.",
-    response_description="A Quote object that contains the quote said by Nicolacus Maximus and the date when the quote was said.",
-    response_model=Quote,
-    openapi_extra={"x-openai-isConsequential": False},
+    "/recipes",
+    summary="Returns a list of recipes.",
+    description="Upon receiving an ingredient, this endpoint will return a list of recipes that contain that ingredient.",
+    response_description="A Document object that contains the recipe and preparation instructions",
+    response_model=list[Document],
+    openapi_extra={
+        "x-openai-isConsequential": False,
+    },
 )
-def get_quote(request: Request):
-    print(request.headers)
-    return {
-        "quote": "Life is short so eat it all.",
-        "year": 1950,
-    }
+def get_recipe(ingredient: str):
+    docs = vector_store.similarity_search(ingredient)
+    return docs
 
 
 user_token_db = {
