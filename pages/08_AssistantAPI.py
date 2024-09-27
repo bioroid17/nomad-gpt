18dc509b3085abd44e708496b5fe986f7d35631e
bioroid17 <trace1997@ajou.ac.kr>
2024-09-27 11:12:14 +0900

    🔥 BONUS: ASSISTANTS API 🔥: Code Challenge
    Code Challenge: Assistant API in streamlit(with streaming)

---
+++ pages/08_AssistantAPI.py
@@ -0,0 +1,249 @@
+import json
+import streamlit as st
+from langchain_community.utilities import DuckDuckGoSearchAPIWrapper
+from typing_extensions import override
+from openai import AssistantEventHandler
+import openai as client
+import yfinance
+
+# # First, we create a EventHandler class to define
+# # how we want to handle the events in the response stream.
+
+ASSISTANT_NAME = "Investor Assistant"
+
+
+class EventHandler(AssistantEventHandler):
+
+    message = ""
+
+    @override
+    def on_text_created(self, text) -> None:
+        self.message_box = st.empty()
+
+    def on_text_delta(self, delta, snapshot):
+        self.message += delta.value
+        self.message_box.markdown(self.message.replace("$", "\$"))
+
+    def on_event(self, event):
+
+        if event.event == "thread.run.requires_action":
+            submit_tool_outputs(event.data.id, event.data.thread_id)
+
+
+st.set_page_config(
+    page_title="AssistantAPI",
+    page_icon="🧰",
+)
+
+st.title("AssistantAPI")
+
+st.markdown(
+    """
+    Welcome to AssistantAPI.
+            
+    Ask a question about a company and our Assistant will do the research for you.
+"""
+)
+
+
+# Tools
+def get_ticker(inputs):
+    ddg = DuckDuckGoSearchAPIWrapper()
+    company_name = inputs["company_name"]
+    return ddg.run(f"Ticker symbol of {company_name}")
+
+
+def get_income_statement(inputs):
+    ticker = inputs["ticker"]
+    stock = yfinance.Ticker(ticker)
+    return json.dumps(stock.income_stmt.to_json())
+
+
+def get_balance_sheet(inputs):
+    ticker = inputs["ticker"]
+    stock = yfinance.Ticker(ticker)
+    return json.dumps(stock.balance_sheet.to_json())
+
+
+def get_daily_stock_performance(inputs):
+    ticker = inputs["ticker"]
+    stock = yfinance.Ticker(ticker)
+    return json.dumps(stock.history(period="3mo").to_json())
+
+
+functions_map = {
+    "get_ticker": get_ticker,
+    "get_income_statement": get_income_statement,
+    "get_balance_sheet": get_balance_sheet,
+    "get_daily_stock_performance": get_daily_stock_performance,
+}
+
+functions = [
+    {
+        "type": "function",
+        "function": {
+            "name": "get_ticker",
+            "description": "Given the name of a company returns its ticker symbol",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "company_name": {
+                        "type": "string",
+                        "description": "The name of the company",
+                    }
+                },
+                "required": ["company_name"],
+            },
+        },
+    },
+    {
+        "type": "function",
+        "function": {
+            "name": "get_income_statement",
+            "description": "Given a ticker symbol (i.e AAPL) returns the company's income statement.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "ticker": {
+                        "type": "string",
+                        "description": "Ticker symbol of the company",
+                    },
+                },
+                "required": ["ticker"],
+            },
+        },
+    },
+    {
+        "type": "function",
+        "function": {
+            "name": "get_balance_sheet",
+            "description": "Given a ticker symbol (i.e AAPL) returns the company's balance sheet.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "ticker": {
+                        "type": "string",
+                        "description": "Ticker symbol of the company",
+                    },
+                },
+                "required": ["ticker"],
+            },
+        },
+    },
+    {
+        "type": "function",
+        "function": {
+            "name": "get_daily_stock_performance",
+            "description": "Given a ticker symbol (i.e AAPL) returns the performance of the stock for the last 100 days.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "ticker": {
+                        "type": "string",
+                        "description": "Ticker symbol of the company",
+                    },
+                },
+                "required": ["ticker"],
+            },
+        },
+    },
+]
+
+
+#### Utilities
+def get_run(run_id, thread_id):
+    return client.beta.threads.runs.retrieve(
+        run_id=run_id,
+        thread_id=thread_id,
+    )
+
+
+def send_message(thread_id, content):
+    return client.beta.threads.messages.create(
+        thread_id=thread_id,
+        role="user",
+        content=content,
+    )
+
+
+def get_messages(thread_id):
+    messages = client.beta.threads.messages.list(thread_id=thread_id)
+    messages = list(messages)
+    messages.reverse()
+    return messages
+
+
+def get_tool_outputs(run_id, thread_id):
+    run = get_run(run_id, thread_id)
+    outputs = []
+    for action in run.required_action.submit_tool_outputs.tool_calls:
+        action_id = action.id
+        function = action.function
+        print(f"Calling function: {function.name} with arg {function.arguments}")
+        outputs.append(
+            {
+                "output": functions_map[function.name](json.loads(function.arguments)),
+                "tool_call_id": action_id,
+            }
+        )
+    return outputs
+
+
+def submit_tool_outputs(run_id, thread_id):
+    outputs = get_tool_outputs(run_id, thread_id)
+    with client.beta.threads.runs.submit_tool_outputs_stream(
+        run_id=run_id,
+        thread_id=thread_id,
+        tool_outputs=outputs,
+        event_handler=EventHandler(),
+    ) as stream:
+        stream.until_done()
+
+
+def insert_message(message, role):
+    with st.chat_message(role):
+        st.markdown(message)
+
+
+def paint_history(thread_id):
+    messages = get_messages(thread_id)
+    for message in messages:
+        insert_message(
+            message.content[0].text.value,
+            message.role,
+        )
+
+
+if "assistant" not in st.session_state:
+    assistants = client.beta.assistants.list(limit=10)
+    for a in assistants:
+        if a.name == ASSISTANT_NAME:
+            assistant = client.beta.assistants.retrieve(a.id)
+            break
+    else:
+        assistant = client.beta.assistants.create(
+            name=ASSISTANT_NAME,
+            instructions="You help users do research on the given query using search engines. You give users the summarization of the information you got.",
+            model="gpt-4o-mini",
+            tools=functions,
+        )
+    thread = client.beta.threads.create()
+    st.session_state["assistant"] = assistant
+    st.session_state["thread"] = thread
+else:
+    assistant = st.session_state["assistant"]
+    thread = st.session_state["thread"]
+
+paint_history(thread.id)
+content = st.chat_input("What do you want to search?")
+if content:
+    send_message(thread.id, content)
+    insert_message(content, "user")
+
+    with st.chat_message("assistant"):
+        with client.beta.threads.runs.stream(
+            thread_id=thread.id,
+            assistant_id=assistant.id,
+            event_handler=EventHandler(),
+        ) as stream:
+            stream.until_done()
